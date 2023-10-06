6fa0c14c4abbe78710c6bf9a55a962750cd02d02
Odafe Aror <odafe41@gmail.com>
2023-10-06 23:57:54 +0000

    first pass

---
+++ src/app/publish/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function Jud() {
+  return (<>
+  <h2>Judy show</h2>
+  <h2>Judy show</h2>
+  <h2>Judy show</h2>
+  <h2>Judy show</h2>
+  <h2>Judy show</h2>
+  <h2>Judy show</h2>
+  <h2>Judy show</h2>
+  <h2>Judy show</h2>
+  <h2>Judy show</h2>
+  </>);
+}
